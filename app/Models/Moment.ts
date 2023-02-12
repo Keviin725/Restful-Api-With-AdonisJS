c9be45812c6c8e8a38b6eddb224ea0db9bc26cdf
Kevin Machava <kevinmachava20@gmail.com>
2023-02-12 15:21:45 +0200

    Update Moment.ts

--- app/Models/Moment.ts
+++ app/Models/Moment.ts
@@ -1,7 +1,10 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Comment from './Comment'
 
 export default class Moment extends BaseModel {
+  @hasMany(() => Comment)
+  public comments: HasMany<typeof Comment>
   @column({ isPrimary: true })
   public id: number
 
